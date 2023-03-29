b3cb41c2f0ff0f5b2e4361c39d2fbc335a597145
vishal singh <vishal1997official@gmail.com>
2023-03-29 17:36:32 +0530

    second commit

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import useFetch from "../../../hooks/useFetch";
 
 const Trending = () => {
-  return (
-    <div>Trending</div>
-  )
-}
+  const [endPoint, setEndPoint] = useState("day");
 
-export default Trending
+  const { data, loading } = useFetch(`/trending/all/${endPoint}`);
+
+  const onTabChange = (tab) => {
+    setEndPoint(tab === "Day" ? "day" : "week");
+  };
+
+  return (
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Trending</span>
+        {/* sending data & onTabChange as props in SwitchTabs component  */}
+        <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default Trending;

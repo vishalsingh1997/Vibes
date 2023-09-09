a26cc3ba70fdf3e3680fe1fe516105b00b54d931
vishal singh <vishal1997official@gmail.com>
2023-09-09 12:50:24 +0530

    logo & toggle button updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,9 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/movix-logo.svg";
+// import logo from "../../assets/movix-logo.svg";
+// import logo from "../../assets/vibesLogo.png";
+import logo from "../../assets/vibes_logo.png";
 
 const Header = () => {
   const [show, setShow] = useState("top");
